86f75a5f7c400fae93925ff5ccaa3e0e32b84202
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-07-17 22:13:09 +0200

    Async errors handling

--- errorHandling/main.go
+++ errorHandling/main.go
@@ -14,4 +14,16 @@ func main() {
 			fmt.Println(cat, "(no such category)")
 		}
 	}
+
+	channel := make(chan ChannelMessage, 10)
+
+	go Products.TotalPriceAsync(categories, channel)
+
+	for message := range channel {
+		if message.CategoryError == nil {
+			fmt.Println(message.Category, "Total:", ToCurrency(message.Total))
+		} else {
+			fmt.Println(message.Category, "(no such category)")
+		}
+	}
 }
