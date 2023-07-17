6baffa59ef9f97239948760304444068cc1e277b
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-07-17 18:29:22 +0200

    Receiving an unknown number of values through channel and closing channel

--- concurrency/main.go
+++ concurrency/main.go
@@ -5,7 +5,18 @@ import (
 )
 
 func main() {
-	fmt.Println("main function started")
-	CalcStoreTotal(Products)
-	fmt.Println("main function complete")
+	// fmt.Println("main function started")
+	// CalcStoreTotal(Products)
+	// fmt.Println("main function complete")
+
+	dispatchChannel := make(chan DispatchNotification, 100)
+	go DispatchOrders(dispatchChannel)
+	for {
+		if details, open := <-dispatchChannel; open {
+			fmt.Println("Dispatch to", details.Customer, ":", details.Quantity, "x", details.Product.Name)
+		} else {
+			fmt.Println("Channel has been closed")
+			break
+		}
+	}
 }
