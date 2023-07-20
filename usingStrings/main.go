08d91f0f274ebdee0dc4d2269e9fb594ca7c0164
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-07-20 18:29:20 +0200

    Learning about formatting strings

--- usingStrings/main.go
+++ usingStrings/main.go
@@ -2,7 +2,26 @@ package main
 
 import "fmt"
 
+func getProductName(index int) (name string, err error) {
+	if len(Products) > index {
+		name = fmt.Sprintf("Name of product: %v", Products[index].Name)
+	} else {
+		err = fmt.Errorf("Error for index %v", index)
+	}
+	return
+}
+
 func main() {
 	fmt.Println("Product:", Kayak.Name, "Price:", Kayak.Price)
 	fmt.Print("Product:", Kayak.Name, "Price:", Kayak.Price, "\n")
+
+	fmt.Printf("Product: %v, Price: $%4.2f", Kayak.Name, Kayak.Price)
+
+	productName, err := getProductName(10)
+
+	if err == nil {
+		fmt.Println(productName)
+	} else {
+		fmt.Println(err.Error())
+	}
 }
