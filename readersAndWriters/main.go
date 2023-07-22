bd7899ba9600688de43aeb64332edd0104743383
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-07-22 21:35:44 +0200

    Learn about writers

--- readersAndWriters/main.go
+++ readersAndWriters/main.go
@@ -26,11 +26,12 @@ var Products = []Product{
 	{"Bling-Bling King", "Chess", 1200},
 }
 
-func processData(reader io.Reader) {
+func processData(reader io.Reader, writer io.Writer) {
 	b := make([]byte, 2)
 	for {
 		count, err := reader.Read(b)
 		if count > 0 {
+			writer.Write(b[0:count])
 			Printfln("Read %v bytes: %v", count, string(b[0:count]))
 		}
 		if err == io.EOF {
@@ -39,7 +40,20 @@ func processData(reader io.Reader) {
 	}
 }
 
+func copyData(reader io.Reader, writer io.Writer) {
+	count, err := io.Copy(writer, reader)
+	if err == nil {
+		Printfln("Read %v bytes", count)
+	} else {
+		Printfln("Error: %v", err.Error())
+	}
+}
+
 func main() {
+	var builder strings.Builder
 	r := strings.NewReader("Kayak")
-	processData(r)
+	// processData(r, &builder)
+	// Printfln("String builder contents: %s", builder.String())
+	copyData(r, &builder)
+	Printfln("String builder contents from copy: %s", builder.String())
 }
