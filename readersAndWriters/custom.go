574c914b693a76c17af41555070978136463fb6c
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-07-23 16:41:12 +0200

    Learn about buffered writer

--- readersAndWriters/custom.go
+++ readersAndWriters/custom.go
@@ -20,3 +20,27 @@ func (cr *CustomReader) Read(slice []byte) (count int, err error) {
 	}
 	return
 }
+
+type CustomWriter struct {
+	writer     io.Writer
+	writeCount int
+}
+
+func NewCustomerWriter(writer io.Writer) *CustomWriter {
+	return &CustomWriter{writer, 0}
+}
+
+func (cw *CustomWriter) Write(slice []byte) (count int, err error) {
+	count, err = cw.writer.Write(slice)
+	cw.writeCount++
+	Printfln("Custom Writer: %v bytes", count)
+	return
+}
+
+func (cw *CustomWriter) Close() (err error) {
+	if closer, ok := cw.writer.(io.Closer); ok {
+		closer.Close()
+	}
+	Printfln("Total writes: %v", cw.writeCount)
+	return
+}
