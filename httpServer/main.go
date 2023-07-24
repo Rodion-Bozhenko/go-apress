008cd22c60373206107b5f778cd635e76fe33993
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-07-24 21:18:16 +0200

    Learn about https requests

--- httpServer/main.go
+++ httpServer/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io"
 	"net/http"
+	"strings"
 )
 
 type StringHandler struct {
@@ -14,12 +15,27 @@ func (sh StringHandler) ServeHTTP(writer http.ResponseWriter, request *http.Requ
 	io.WriteString(writer, sh.message)
 }
 
+func HTTPSRedirect(writer http.ResponseWriter, request *http.Request) {
+	host := strings.Split(request.Host, ":")[0]
+	target := "https://" + host + ":5500" + request.URL.Path
+	if len(request.URL.RawQuery) > 0 {
+		target += "?" + request.URL.RawQuery
+	}
+	http.Redirect(writer, request, target, http.StatusTemporaryRedirect)
+}
 func main() {
 	http.Handle("/message", StringHandler{"Hello World"})
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.Handle("/", http.RedirectHandler("/message", http.StatusTemporaryRedirect))
 
-	err := http.ListenAndServe("localhost:5000", nil)
+	go func() {
+		err := http.ListenAndServeTLS(":5500", "certificate.cer", "certificate.key", nil)
+		if err != nil {
+			Printfln("HTTPS Error: %v", err.Error())
+		}
+	}()
+
+	err := http.ListenAndServe("localhost:5000", http.HandlerFunc(HTTPSRedirect))
 	if err != nil {
 		Printfln("Error: %v", err.Error())
 	}
