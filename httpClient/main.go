d71e5089eec94076df42d68fe0b4d974ffceb5a1
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-08-05 19:33:19 +0200

    Creating multipart forms

--- httpClient/main.go
+++ httpClient/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"io"
+	"mime/multipart"
 	"net/http"
-	"net/url"
 	"os"
 	"time"
 )
@@ -138,14 +139,50 @@ func main() {
 	// 	}
 	// }
 
-	http.DefaultClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
-		if len(via) == 3 {
-			url, _ := url.Parse("http://localhost:5000/html")
-			req.URL = url
-		}
-		return nil
+	// http.DefaultClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+	// 	if len(via) == 3 {
+	// 		url, _ := url.Parse("http://localhost:5000/html")
+	// 		req.URL = url
+	// 	}
+	// 	return nil
+	// }
+	// req, err := http.NewRequest(http.MethodGet, "http://localhost:5000/redirect1", nil)
+	// if err == nil {
+	// 	var response *http.Response
+	// 	response, err = http.DefaultClient.Do(req)
+	// 	if err == nil {
+	// 		io.Copy(os.Stdout, response.Body)
+	// 	} else {
+	// 		Printfln("Request Error: %v", err.Error())
+	// 	}
+	// } else {
+	// 	Printfln("Error: %v", err.Error())
+	// }
+
+	var buffer bytes.Buffer
+	formWriter := multipart.NewWriter(&buffer)
+	fieldWriter, err := formWriter.CreateFormField("name")
+	if err == nil {
+		io.WriteString(fieldWriter, "Alice")
 	}
-	req, err := http.NewRequest(http.MethodGet, "http://localhost:5000/redirect1", nil)
+	fieldWriter, err = formWriter.CreateFormField("city")
+	if err == nil {
+		io.WriteString(fieldWriter, "New York")
+	}
+	fileWriter, err := formWriter.CreateFormFile("codeFile", "printer.go")
+	if err == nil {
+		fileData, err := os.ReadFile("./printer.go")
+		if err == nil {
+			fileWriter.Write(fileData)
+		}
+	}
+
+	formWriter.Close()
+
+	req, err := http.NewRequest(http.MethodPost, "http://localhost:5000/form", &buffer)
+
+	req.Header["Content-Type"] = []string{formWriter.FormDataContentType()}
+
 	if err == nil {
 		var response *http.Response
 		response, err = http.DefaultClient.Do(req)
