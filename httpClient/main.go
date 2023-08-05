fff9046bafaf7777a6a710cc1d341d1e21921a10
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-08-05 17:37:42 +0200

    Learn about redirects

--- httpClient/main.go
+++ httpClient/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"net/http"
-	"net/http/cookiejar"
+	"net/url"
 	"os"
 	"time"
 )
@@ -113,29 +112,49 @@ func main() {
 	// 	Printfln("Request Init Error: %v", err.Error())
 	// }
 
-	clients := make([]http.Client, 3)
-	for index, client := range clients {
-		jar, err := cookiejar.New(nil)
-		if err == nil {
-			// http.DefaultClient.Jar = jar
-			client.Jar = jar
-		}
+	// clients := make([]http.Client, 3)
+	// for index, client := range clients {
+	// 	jar, err := cookiejar.New(nil)
+	// 	if err == nil {
+	// 		// http.DefaultClient.Jar = jar
+	// 		client.Jar = jar
+	// 	}
+	//
+	// 	for i := 0; i < 3; i++ {
+	// 		req, err := http.NewRequest(http.MethodGet, "http://localhost:5000/cookie", nil)
+	// 		if err == nil {
+	// 			// response, err := http.DefaultClient.Do(req)
+	// 			response, err := client.Do(req)
+	// 			if err == nil && response.StatusCode == http.StatusOK {
+	// 				fmt.Fprintf(os.Stdout, "Client %v: ", index)
+	// 				io.Copy(os.Stdout, response.Body)
+	// 				defer response.Body.Close()
+	// 			} else {
+	// 				Printfln("Request Error: %v", err.Error())
+	// 			}
+	// 		} else {
+	// 			Printfln("Request Init Error: %v", err.Error())
+	// 		}
+	// 	}
+	// }
 
-		for i := 0; i < 3; i++ {
-			req, err := http.NewRequest(http.MethodGet, "http://localhost:5000/cookie", nil)
-			if err == nil {
-				// response, err := http.DefaultClient.Do(req)
-				response, err := client.Do(req)
-				if err == nil && response.StatusCode == http.StatusOK {
-					fmt.Fprintf(os.Stdout, "Client %v: ", index)
-					io.Copy(os.Stdout, response.Body)
-					defer response.Body.Close()
-				} else {
-					Printfln("Request Error: %v", err.Error())
-				}
-			} else {
-				Printfln("Request Init Error: %v", err.Error())
-			}
+	http.DefaultClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		if len(via) == 3 {
+			url, _ := url.Parse("http://localhost:5000/html")
+			req.URL = url
 		}
+		return nil
+	}
+	req, err := http.NewRequest(http.MethodGet, "http://localhost:5000/redirect1", nil)
+	if err == nil {
+		var response *http.Response
+		response, err = http.DefaultClient.Do(req)
+		if err == nil {
+			io.Copy(os.Stdout, response.Body)
+		} else {
+			Printfln("Request Error: %v", err.Error())
+		}
+	} else {
+		Printfln("Error: %v", err.Error())
 	}
 }
