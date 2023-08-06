096838a5e2c828b52621d759de5eacedd73ca99d
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-08-06 20:24:29 +0200

    Mutex and RWMutex

--- coordination/main.go
+++ coordination/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"math"
+	"math/rand"
 	"sync"
 	"time"
 )
 
 var waitGroup = sync.WaitGroup{}
 var mutex = sync.Mutex{}
+var rwmutex = sync.RWMutex{}
+var squares = map[int]int{}
 
 func doSum(count int, val *int) {
 	time.Sleep(time.Second)
@@ -18,14 +22,37 @@ func doSum(count int, val *int) {
 	waitGroup.Done()
 }
 
+func calculateSquares(max, iterations int) {
+	for i := 0; i < iterations; i++ {
+		val := rand.Intn(max)
+		rwmutex.RLock()
+		square, ok := squares[val]
+		rwmutex.RUnlock()
+		if ok {
+			Printfln("Added value: %v = %v", val, square)
+		} else {
+			rwmutex.Lock()
+			if _, ok := squares[val]; !ok {
+				squares[val] = int(math.Pow(float64(val), 2))
+				Printfln("Added value: %v = %v", val, squares[val])
+			}
+			rwmutex.Unlock()
+		}
+	}
+	waitGroup.Done()
+}
+
 func main() {
-	counter := 0
+	// counter := 0
+	rand.Seed(time.Now().UnixNano())
 
 	numRoutines := 3
 	waitGroup.Add(numRoutines)
 	for i := 0; i < numRoutines; i++ {
-		go doSum(5000, &counter)
+		// go doSum(5000, &counter)
+		go calculateSquares(10, 5)
 	}
 	waitGroup.Wait()
-	Printfln("Total: %v", counter)
+	// Printfln("Total: %v", counter)
+	Printfln("Cached values: %v", len(squares))
 }
