3db80a47191c176582ff79680831366dbf6af68b
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-08-06 19:40:57 +0200

    Learn about mutex

--- coordination/main.go
+++ coordination/main.go
@@ -1,13 +1,31 @@
 package main
 
+import (
+	"sync"
+	"time"
+)
+
+var waitGroup = sync.WaitGroup{}
+var mutex = sync.Mutex{}
+
 func doSum(count int, val *int) {
+	time.Sleep(time.Second)
+	mutex.Lock()
 	for i := 0; i < count; i++ {
 		*val++
 	}
+	mutex.Unlock()
+	waitGroup.Done()
 }
 
 func main() {
 	counter := 0
-	go doSum(5000, &counter)
+
+	numRoutines := 3
+	waitGroup.Add(numRoutines)
+	for i := 0; i < numRoutines; i++ {
+		go doSum(5000, &counter)
+	}
+	waitGroup.Wait()
 	Printfln("Total: %v", counter)
 }
