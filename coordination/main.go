6faf8c13c997561d08d9a242cadba12727027a9b
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-08-07 19:19:42 +0200

    Using conditions to coordinate goroutine

--- coordination/main.go
+++ coordination/main.go
@@ -11,6 +11,7 @@ var waitGroup = sync.WaitGroup{}
 var mutex = sync.Mutex{}
 var rwmutex = sync.RWMutex{}
 var squares = map[int]int{}
+var readyCond = sync.NewCond(rwmutex.RLocker())
 
 func doSum(count int, val *int) {
 	time.Sleep(time.Second)
@@ -42,16 +43,45 @@ func calculateSquares(max, iterations int) {
 	waitGroup.Done()
 }
 
+func generateSquares(max int) {
+	rwmutex.Lock()
+	Printfln("Generating data...")
+	for val := 0; val < max; val++ {
+		squares[val] = int(math.Pow(float64(val), 2))
+	}
+	rwmutex.Unlock()
+	Printfln("Broadcasting condition")
+	readyCond.Broadcast()
+	waitGroup.Done()
+}
+
+func readSquares(id, max, iterations int) {
+	readyCond.L.Lock()
+	for len(squares) == 0 {
+		readyCond.Wait()
+	}
+	for i := 0; i < iterations; i++ {
+		key := rand.Intn(max)
+		Printfln("#%v Read Value: %v = %v", id, key, squares[key])
+		time.Sleep(time.Millisecond * 100)
+	}
+	readyCond.L.Unlock()
+	waitGroup.Done()
+}
+
 func main() {
 	// counter := 0
 	rand.Seed(time.Now().UnixNano())
 
-	numRoutines := 3
+	numRoutines := 2
 	waitGroup.Add(numRoutines)
 	for i := 0; i < numRoutines; i++ {
 		// go doSum(5000, &counter)
-		go calculateSquares(10, 5)
+		// go calculateSquares(10, 5)
+		go readSquares(i, 10, 5)
 	}
+	waitGroup.Add(1)
+	go generateSquares(10)
 	waitGroup.Wait()
 	// Printfln("Total: %v", counter)
 	Printfln("Cached values: %v", len(squares))
