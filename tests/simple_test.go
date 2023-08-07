7ca34e2943ceb2d78d2a0e4558ea17463f9547ec
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-08-07 22:22:08 +0200

    Start learning about unit testing

---
+++ tests/simple_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	testValues := []int{10, 20, 30}
+	_, sum := sortAndTotal(testValues)
+	expected := 60
+	if sum != expected {
+		t.Fatalf("Expected %v, Got %v", expected, sum)
+	}
+}
