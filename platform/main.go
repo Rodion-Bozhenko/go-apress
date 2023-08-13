c9aadf608b7d986fdb727a56a68608200cc1a9b2
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-08-13 18:48:17 +0200

    Create function for running server

--- platform/main.go
+++ platform/main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	"platform/config"
-	"platform/logging"
+	"platform/placeholder"
 	"platform/services"
 )
 
-func writeMessage(logger logging.Logger, cfg config.Configuration) {
-	section, ok := cfg.GetSection("main")
-	if ok {
-		message, ok := section.GetString("message")
-		if ok {
-			logger.Info(message)
-		} else {
-			logger.Panic("Cannot find configuration setting")
-		}
-	} else {
-		logger.Panic("Config section not found")
-	}
-}
+// func writeMessage(logger logging.Logger, cfg config.Configuration) {
+// 	section, ok := cfg.GetSection("main")
+// 	if ok {
+// 		message, ok := section.GetString("message")
+// 		if ok {
+// 			logger.Info(message)
+// 		} else {
+// 			logger.Panic("Cannot find configuration setting")
+// 		}
+// 	} else {
+// 		logger.Panic("Config section not found")
+// 	}
+// }
 
 func main() {
 	services.RegisterDefaultServices()
+	placeholder.Start()
 
 	// var cfg config.Configuration
 	// services.GetService(&cfg)
@@ -30,14 +30,14 @@ func main() {
 	// services.GetService(&logger)
 
 	// writeMessage(logger, cfg)
-	services.Call(writeMessage)
-
-	val := struct {
-		message string
-		logging.Logger
-	}{
-		message: "Hello from the struct",
-	}
-	services.Populate(&val)
-	val.Logger.Debug(val.message)
+	// services.Call(writeMessage)
+	//
+	// val := struct {
+	// 	message string
+	// 	logging.Logger
+	// }{
+	// 	message: "Hello from the struct",
+	// }
+	// services.Populate(&val)
+	// val.Logger.Debug(val.message)
 }
